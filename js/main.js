70958dd8d98fe3d1bc8b6a05102a1666eb54d06a
Matias Pinto <matiassebastianpinto1988@gmail.com>
2023-11-01 10:11:50 -0300

    agregando un for

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@ const lavandina = 300
 const jabonLiquido = 350
 const perfumina = 150
 
-function bienvenido(){
+function bienvenido() {
     let producto = prompt("¡Bienvenido! Seleccione un producto ingresando un numero entre 1 y 4" + "\n" + "\n1. Cloro" + "\n2. Lavandina" + "\n3. Jabon liquido" + "\n4. Perfumina")
-    if ((producto > 0) && (producto < 5)){
-        calcularPrecio(producto) 
-    }else {
-        alert ("El valor ingresado no es correcto, reintentar.")
+
+    if ((producto < 1) || (producto > 4)) {
+        alert("El valor ingresado no es correcto, reintentar.")
         bienvenido()
-    }   
-       
+    } else {
+        for (let i = 0; i <= 4; i++) {
+            if (producto == i) {
+                calcularPrecio(producto)
+                break;
+            }
+        }
+    }
 }
+
 function calcularPrecio(producto){
     let articulo
     let valor
