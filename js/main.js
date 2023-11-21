6dabb09ce28472cc0b976f2263b31a70aaaa7847
Matias Pinto <matiassebastianpinto1988@gmail.com>
2023-11-20 22:04:58 -0300

    agregando clases y arrays

--- js/main.js
+++ js/main.js
@@ -1,45 +1,44 @@
-const cloro = 200
-const lavandina = 300
-const jabonLiquido = 350
-const perfumina = 150
+const articulo = function (nombre,precio){
+    this.nombre = nombre
+    this.precio = precio
+}
+
+let articulo1 = new articulo ("cloro", 200)
+let articulo2 = new articulo ("lavandina", 300)
+let articulo3 = new articulo ("jabon liquido", 350)
+let articulo4 = new articulo ("perfumina para piso", 150)
+
+let articulos = [articulo1,articulo2,articulo3,articulo4]
+
 
 function bienvenido() {
-    let producto = prompt("¡Bienvenido! Seleccione un producto ingresando un numero entre 1 y 4" + "\n" + "\n1. Cloro" + "\n2. Lavandina" + "\n3. Jabon liquido" + "\n4. Perfumina")
+    let producto = parseInt(prompt("¡Bienvenido! Seleccione un producto ingresando un numero entre 1 y 4" + "\n" + "\n1. Cloro" + "\n2. Lavandina" + "\n3. Jabon liquido" + "\n4. Perfumina"))
 
-    if ((producto < 1) || (producto > 4)) {
-        alert("El valor ingresado no es correcto, reintentar.")
-        bienvenido()
-    } else {
-        for (let i = 0; i <= 4; i++) {
-            if (producto == i) {
-                calcularPrecio(producto)
-                break;
+    if (!Number.isInteger(producto)) {
+        alert("El valor ingresado no es un numero")
+    }else{
+        if ((producto < 1) || (producto > 4)) {
+            alert("Por favor, ingresar un numero entre 1 y 4.")
+            bienvenido()
+        } else {
+            for (let i= 0; i < articulos.length; i++) {
+                if (producto == i+1) {
+                    calcular(articulos[i])
+                    break;
+                }
             }
         }
     }
 }
-
-function calcularPrecio(producto){
-    let articulo
-    let valor
-    if (producto == 1) {
-        articulo = "Cloro"
-        valor = cloro
+function calcular(articulo){    
+    let cantidad = parseInt(prompt("Ingrese cuantos litros de " + articulo.nombre + " desea comprar"))
+    if (!Number.isInteger(cantidad)) {
+        alert("El valor ingresado no es un numero")
+        calcular(articulo)
+    }else{
+        let resultado = cantidad * articulo.precio
+        alert ("Estas comprando " + cantidad + "L de " + articulo.nombre + "\nEl valor de su compra es " + "$" + resultado)
     }
-    else if (producto == 2) {
-        articulo = "Lavandina"
-        valor = lavandina
-    }
-    else if (producto == 3) {
-        articulo = "Jabon Liquido"
-        valor = jabonLiquido
-    }
-    else {
-        articulo = "Perfumina"
-        valor = perfumina
-    }
-    let cantidad = prompt("Ingrese cuantos litros de " + articulo + " desea comprar")
-    let resultado = cantidad * valor
-    alert ("Estas comprando " + cantidad + "L de " + articulo + "\nEl valor de su compra es " + "$" + resultado)
 }
+
 bienvenido()
